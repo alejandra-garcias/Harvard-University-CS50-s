91436389b369e29e0c70665c41cab0acaafe9327
alejandra-garcias <alejandra.garsanchez@gmail.com>
2023-11-23 13:01:02 +0100

    archive logic added

--- CS50W-Web Programming with Python and JS/mail/mail/mail/static/mail/inbox.js
+++ CS50W-Web Programming with Python and JS/mail/mail/mail/static/mail/inbox.js
@@ -59,66 +59,71 @@ function load_mailbox(mailbox) {
 
   // Display the mailbox
   fetch(`/emails/${mailbox}`)
-    .then(response => response.json())
-    .then(emails => {
-      const emailsView = document.querySelector('#emails-view');
-      emailsView.classList.add('emails-view');
+  .then(response => response.json())
+  .then(emails => {
+    const emailsView = document.querySelector('#emails-view');
+    emailsView.classList.add('emails-view');
 
-      if (emails.length > 0) {
-        emails.forEach(email => {
-          const emailDiv = document.createElement('div');
-          emailDiv.classList.add('email-preview');
-          emailDiv.innerHTML = `
-            <p>From: ${email.sender}</p>
-            <p>Subject: ${email.subject}</p>
-            <p>Body: ${email.body}</p>
-          `;
+    if (emails.length > 0) {
+      emails.forEach(email => {
+        const emailDiv = document.createElement('div');
+        emailDiv.classList.add('email-preview');
+        emailDiv.innerHTML = `
+          <p>From: ${email.sender}</p>
+          <p>Subject: ${email.subject}</p>
+          <p>Body: ${email.body}</p>
+        `;
 
+        // Display singular email
+        emailDiv.addEventListener('click', function () {
+          let email_id = email.id;
 
-
-          emailDiv.addEventListener('click', function() {
-            let email_id = email.id;
-            fetch(`/emails/${email_id}`)
-            .then(response => response.json())
-            .then (email =>{
-              // Clear the emailsView
-              console.log(email)
-              emailsView.innerHTML = '';
-
-              const innerEmail = document.createElement('div');
-              innerEmail.classList.add('inner-email');
-              innerEmail.innerHTML=
-              `<p>From: ${email.sender}</p>
-              <p>Subject: ${email.subject}</p>
-              <p>Body: ${email.body}</p>`;
-              
-
-              
-              emailsView.appendChild(innerEmail);
-
-
-              
-            })
-
-            if (email.read !== true){
-              fetch(`/emails/${email_id}`, {
-                method: 'PUT',
-                body: JSON.stringify({
-                    read: true
-                })
+          // Mark email as read
+          if (email.read !== true) {
+            fetch(`/emails/${email_id}`, {
+              method: 'PUT',
+              body: JSON.stringify({
+                read: true
               })
-            
-              console.log('Message marked as read')
-            }else{
-              console.log('The message was already read!');
-            }
+            })
+              .then(() => console.log('Message marked as read'))
+              .catch(error => console.error('Error marking message as read:', error));
+          } else {
+            console.log('The message was already read!');
+          }
 
-        
-            
+          // Clear the emailsView
+          emailsView.innerHTML = '';
+
+          const innerEmail = document.createElement('div');
+          innerEmail.classList.add('inner-email');
+          innerEmail.innerHTML =
+            `<p>From: ${email.sender}</p>
+            <p>Subject: ${email.subject}</p><p>${email.timestamp}</p>
+            <p>Body: ${email.body}</p>`;
+
+          // Archive/Unarchive logic
+          const btn_arch = document.createElement('button');
+          btn_arch.innerHTML = email.archived ? "Unarchive" : "Archive";
+          btn_arch.className = email.archived ? "btn btn-success" : "btn btn-danger";
+          btn_arch.addEventListener('click', function () {
+            fetch(`/emails/${email_id}`, {
+              method: 'PUT',
+              body: JSON.stringify({
+                archived: !email.archived
+              })
+            })
+              .then(() => {
+                load_mailbox('archive');
+              })
+              .catch(error => console.error('Error archiving/unarchiving message:', error));
           });
-
-          emailsView.appendChild(emailDiv);
+          emailsView.appendChild(innerEmail);
+          emailsView.appendChild(btn_arch);
         });
+
+        emailsView.appendChild(emailDiv);
+      });
       } else {
         const emptyElement = document.createElement('div');
         emptyElement.innerHTML = 'No hay emails para mostrar';
@@ -131,3 +136,4 @@ function load_mailbox(mailbox) {
 
   return false;
 }
+
